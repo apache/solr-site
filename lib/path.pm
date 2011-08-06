8e588eaf1633cc21154d7359ff7cdccad58b0e44
Grant Ingersoll <gsingers@apache.org>
2011-08-06 23:59:38 +0000

    flesh out the lucene core site some more
    git-svn-id: https://svn.apache.org/repos/asf/lucene/cms/trunk@1154625 13f79535-47bb-0310-9956-ffa450edef68

--- lib/path.pm
+++ lib/path.pm
@@ -6,11 +6,11 @@ use ASF::Value;
 our @patterns = (
 
 #	[qr!/sitemap\.html$!, sitemap => { headers => { title => "Lucene Sitemap" }} ],
+	[qr!core\/.*?\.mdtext$!, main => { template => "core.html" }],
 	[qr!\.mdtext$!, main => { template => "main.html" }],
-	[qr!core\.mdtext$!, main => { template => "core.html" }],
-	[qr!solr\.mdtext$!, main => { template => "solr.html" }],
-	[qr!pylucene\.mdtext$!, main => { template => "pylucene.html" }],
-	[qr!openrelevance\.mdtext$!, main => { template => "orp.html" }],
+	[qr!solr/\.mdtext$!, main => { template => "solr.html" }],
+	[qr!pylucene/\.mdtext$!, main => { template => "pylucene.html" }],
+	[qr!openrelevance/\.mdtext$!, main => { template => "orp.html" }],
 
 ) ;
 
