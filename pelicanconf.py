481875d098d1975b2f667e37e143b79bec95ce79
Bruno Roustant <broustant@salesforce.com>
2020-07-14 15:43:36 +0200

    Update version variables for release 8.6.0

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,8 +10,8 @@ SITESUBTITLE = ''
 SITEURL = ''
 CURRENTYEAR = date.today().year
 
-LUCENE_LATEST_RELEASE = '8.5.2'
-LUCENE_LATEST_RELEASE_DATE = datetime(2020, 5, 26)
+LUCENE_LATEST_RELEASE = '8.6.0'
+LUCENE_LATEST_RELEASE_DATE = datetime(2020, 7, 15)
 LUCENE_PREVIOUS_MAJOR_RELEASE = '7.7.3'
 
 # This string will be appended to all unversioned css and js resources to prevent caching surprises on edits.
