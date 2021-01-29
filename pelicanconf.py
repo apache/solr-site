88735beb167756b2ac5335a74a1a671ac26f8305
noblepaul <noble.paul@gmail.com>
2021-01-29 17:28:59 +1100

    Update version variables for release 8.8.0

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,8 +10,8 @@ SITESUBTITLE = ''
 SITEURL = ''
 CURRENTYEAR = date.today().year
 
-LUCENE_LATEST_RELEASE = '8.7.0'
-LUCENE_LATEST_RELEASE_DATE = datetime(2020, 11, 3)
+LUCENE_LATEST_RELEASE = '8.8.0'
+LUCENE_LATEST_RELEASE_DATE = datetime(2021, 01, 29)
 LUCENE_PREVIOUS_MAJOR_RELEASE = '7.7.3'
 
 # This string will be appended to all unversioned css and js resources to prevent caching surprises on edits.
